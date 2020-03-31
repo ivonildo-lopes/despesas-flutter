52900467e7d04ee3a83416a1931e5dcece0b9dc9
ivonildo lopes rodrigues <ivonildolopes.ti@gmail.com>
2020-03-30 22:28:51 -0300

    criando o model transaction

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:despesas/model/transaction.dart';
 import 'package:flutter/material.dart';
 
 main() => runApp(Despesa());
@@ -12,19 +13,28 @@ class Despesa  extends StatelessWidget {
 }
 
 class HomePage  extends StatelessWidget {
+   
+   final _transaction = [
+      Transaction(id: '1', title: 'cinema', valor: 105, date: DateTime.now()),
+      Transaction(id: '2', title: 'compras', valor: 975.50, date: DateTime.now()),
+      Transaction(id: '3', title: 'boneco henry', valor: 79.99, date: DateTime.now()),
+      Transaction(id: '4', title: 'lanche', valor: 45.99, date: DateTime.now()),
+   ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Controle de despesas'),
       ),
-      body: Column(
-        // crossAxisAlignment: CrossAxisAlignment.start, // esquerda
+      body: Column(       
         // crossAxisAlignment: CrossAxisAlignment.center, // default
-        // crossAxisAlignment: CrossAxisAlignment.end, // direita
+        // mainAxisAlignment: MainAxisAlignment.start, // default
+        mainAxisAlignment: MainAxisAlignment.spaceAround, 
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
           Container(
-            width: double.infinity,
+            // width: double.infinity,
             height: 100,
             child: Card(
               child: Text('AQUI FICARA O GRAFICO'),
