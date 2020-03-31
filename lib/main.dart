7f2f512a2456495a283465a426999937d57c9c72
ivonildo lopes rodrigues <ivonildolopes.ti@gmail.com>
2020-03-30 22:14:14 -0300

    criando column da homePage

--- lib/main.dart
+++ lib/main.dart
@@ -18,8 +18,24 @@ class HomePage  extends StatelessWidget {
       appBar: AppBar(
         title: Text('Controle de despesas'),
       ),
-      body: Center(
-        child: Text('Versao inicial'),
+      body: Column(
+        // crossAxisAlignment: CrossAxisAlignment.start, // esquerda
+        // crossAxisAlignment: CrossAxisAlignment.center, // default
+        // crossAxisAlignment: CrossAxisAlignment.end, // direita
+        children: <Widget>[
+          Container(
+            width: double.infinity,
+            height: 100,
+            child: Card(
+              child: Text('AQUI FICARA O GRAFICO'),
+              color: Colors.grey[350],
+              elevation: 5,
+            ),
+          ),
+          Card(
+            child: Text('AQUI FICARA A LISTA DE GASTOS'),
+          ),
+        ],
       ),
     );
   }
