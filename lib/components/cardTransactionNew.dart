d5ba79e0372bafc7abac229c72c1b80f03e4d747
ivonildo lopes rodrigues <ivonildolopes.ti@gmail.com>
2020-04-01 02:09:07 -0300

    teclado numerico e valida campos

--- lib/components/cardTransactionNew.dart
+++ lib/components/cardTransactionNew.dart
@@ -1,52 +1,56 @@
 import 'package:flutter/material.dart';
 
-class CardTransactionNew  extends StatelessWidget {
-   
-   final titleControlher = TextEditingController();
-   final valueControlher = TextEditingController();
+class CardTransactionNew extends StatelessWidget {
+  final titleControlher = TextEditingController();
+  final valueControlher = TextEditingController();
 
   final void Function(String, double) addTransaction;
 
-   CardTransactionNew(this.addTransaction);
+  CardTransactionNew(this.addTransaction);
+
+  _submit() {
+
+    final String title = titleControlher.text;
+    final double value  = double.tryParse(valueControlher.text) ?? 0;
+
+    if(title.isEmpty || value < 0) return;
+
+    this.addTransaction(title,value);
+  }
 
   @override
   Widget build(BuildContext context) {
     return Card(
-            elevation: 5,
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Column(
-                children: <Widget>[
-                  TextField(
-                    // onChanged: (newValue) => title = newValue,
-                    controller: this.titleControlher,
-                    decoration: InputDecoration(
-                      labelText: 'Titulo'
-                    ),
-                  ),
-                  TextField(
-                    // onChanged: (newValue) => value = newValue,
-                    controller: this.valueControlher,
-                    decoration: InputDecoration(
-                      labelText: 'Valor R\$',
-                    ),
-                  ),
-                  FlatButton( 
-                    child: Text('Nova Transacao',
-                      style: TextStyle(
-                        color: Colors.purple
-                      ),
-                    ),
-                    onPressed: () {
-                      this.addTransaction(
-                        titleControlher.text,
-                        double.tryParse(valueControlher.text) ?? 0
-                      );
-                    },
-                  )
-                ],
+      elevation: 5,
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: <Widget>[
+            TextField(
+              controller: this.titleControlher,
+              onSubmitted: (_) => _submit(),
+              decoration: InputDecoration(labelText: 'Titulo'),
+            ),
+            TextField(
+              controller: this.valueControlher,
+              keyboardType: TextInputType.numberWithOptions(decimal: true),
+              onSubmitted: (_) => _submit(),
+              decoration: InputDecoration(
+                labelText: 'Valor R\$',
               ),
             ),
-          );
+            FlatButton(
+              child: Text(
+                'Nova Transacao',
+                style: TextStyle(color: Colors.purple),
+              ),
+              onPressed: () {
+                _submit();
+              },
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
